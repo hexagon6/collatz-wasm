cab52fa7456484939d87c64cf13397d04b8a89f8
Hexagon Six <hexagon6@fet.li>
2021-10-09 23:40:40 +0200

    release: add pkg

---
+++ pkg/README.md
@@ -0,0 +1,65 @@
+# collatz-wasm
+
+1. pick a positive integer number
+2. a) when odd: 3x + 1
+2. b) when even: x/2
+3. repeat until you find a number you had before
+
+on positive integers this algorithm (the mathematical equivalent is called [Collatz Conjecture](https://en.wikipedia.org/wiki/Collatz_conjecture)) ends always at 1
+
+![XKCD Collatz Conjecture](https://imgs.xkcd.com/comics/collatz_conjecture.png)
+
+> it is an unsolved mathematical problem, but you could give it a try in the browser or another system which can run wasm with this module ;-)
+
+> it accepts any javascript number (internally i32) and gives back an Array of numbers
+
+## usage
+
+By installing from [npm](https://www.npmjs.com/package/collatz-wasm)
+
+`npm install collatz-wasm`
+
+and by using a bundler you should be able to import it in javascript like this:
+```javascript
+import init, { collatz } from "collatz-wasm"
+
+...
+
+init()
+   .then(() => {
+      collatz(1337)
+   })
+```
+
+## plans for the future
+
+I plan on adding support for i64 (javascript BigInt), but have not yet reached a proper understanding of Rust Traits to do so. An alternative would be to copy code and replace i32 with i64, but who would do such a thing??
+
+## uses
+
+The Collatz Conjecture can be used to plot nice graphs and art, which was a motivation to create this module
+
+## building from rust to wasm
+### install dependencies
+First install a rust environment (e.g. rustup)
+and then install wasm-pack like-a-so: `$ cargo install wasm-pack`
+### build wasm bundle
+> based on this howto: https://developer.mozilla.org/en-US/docs/WebAssembly/Rust_to_wasm
+
+`$ wasm-pack build --target web`
+
+This builds the rust lib.rs into a file bundle at `pkg/` which is ready to be imported into a javascript environment that has wasm support.
+
+### using it locally from the git repository
+
+```javascript
+ import init, { collatz } from "./pkg/collatz_wasm.js"
+ init()
+   .then(() => {
+      const list = Array.from(collatz(10))
+      console.log(JSON.stringify(list))
+   })
+```
+
+### test wasm bundle locally
+Start a local webserver (e.g. `python3 -m http.server`) and navigate to index.html to see a test page importing the compiled wasm (open Developer Tools to see result logged in the Javascript console).
---
+++ pkg/collatz_wasm.js
@@ -0,0 +1,86 @@
+
+let wasm;
+
+let cachegetInt32Memory0 = null;
+function getInt32Memory0() {
+    if (cachegetInt32Memory0 === null || cachegetInt32Memory0.buffer !== wasm.memory.buffer) {
+        cachegetInt32Memory0 = new Int32Array(wasm.memory.buffer);
+    }
+    return cachegetInt32Memory0;
+}
+
+function getArrayI32FromWasm0(ptr, len) {
+    return getInt32Memory0().subarray(ptr / 4, ptr / 4 + len);
+}
+/**
+* @param {number} x
+* @returns {Int32Array}
+*/
+export function collatz(x) {
+    try {
+        const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
+        wasm.collatz(retptr, x);
+        var r0 = getInt32Memory0()[retptr / 4 + 0];
+        var r1 = getInt32Memory0()[retptr / 4 + 1];
+        var v0 = getArrayI32FromWasm0(r0, r1).slice();
+        wasm.__wbindgen_free(r0, r1 * 4);
+        return v0;
+    } finally {
+        wasm.__wbindgen_add_to_stack_pointer(16);
+    }
+}
+
+async function load(module, imports) {
+    if (typeof Response === 'function' && module instanceof Response) {
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            try {
+                return await WebAssembly.instantiateStreaming(module, imports);
+
+            } catch (e) {
+                if (module.headers.get('Content-Type') != 'application/wasm') {
+                    console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        const bytes = await module.arrayBuffer();
+        return await WebAssembly.instantiate(bytes, imports);
+
+    } else {
+        const instance = await WebAssembly.instantiate(module, imports);
+
+        if (instance instanceof WebAssembly.Instance) {
+            return { instance, module };
+
+        } else {
+            return instance;
+        }
+    }
+}
+
+async function init(input) {
+    if (typeof input === 'undefined') {
+        input = new URL('collatz_wasm_bg.wasm', import.meta.url);
+    }
+    const imports = {};
+
+
+    if (typeof input === 'string' || (typeof Request === 'function' && input instanceof Request) || (typeof URL === 'function' && input instanceof URL)) {
+        input = fetch(input);
+    }
+
+
+
+    const { instance, module } = await load(await input, imports);
+
+    wasm = instance.exports;
+    init.__wbindgen_wasm_module = module;
+
+    return wasm;
+}
+
+export default init;
+
